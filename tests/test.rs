c625847c46cdc087216fa06f540b99efcaaec319
James Cole <james@assimityai.com>
2021-12-28 18:39:45 +0000

    Add logging

--- tests/test.rs
+++ tests/test.rs
@@ -7,5 +7,10 @@ fn run_no_param() {
         .output()
         .expect("failed to execute process");
 
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    let stderr = String::from_utf8(output.stderr).unwrap();
+
+    println!("stdout: {}", stdout);
+    println!("stderr: {}", stderr);
     assert!(output.status.success());
 }
